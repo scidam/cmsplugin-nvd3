d441a48418e2711665be2a8623b2238552dedf07
Kislov Dmitry <kislov@easydan.com>
2016-04-18 08:46:12 +1000

    Remove migration_modules for cms from test_settings

--- cmsplugin_nvd3/tests/test_settings.py
+++ cmsplugin_nvd3/tests/test_settings.py
@@ -220,15 +220,3 @@ DATABASES = {
         'USER': ''
     }
 }
-
-MIGRATION_MODULES = {
-    'djangocms_column': 'djangocms_column.migrations_django',
-    'djangocms_inherit': 'djangocms_inherit.migrations_django',
-    'djangocms_picture': 'djangocms_picture.migrations_django',
-    'djangocms_style': 'djangocms_style.migrations_django',
-    'djangocms_video': 'djangocms_video.migrations_django',
-    'djangocms_file': 'djangocms_file.migrations_django',
-    'djangocms_teaser': 'djangocms_teaser.migrations_django',
-    'djangocms_googlemap': 'djangocms_googlemap.migrations_django',
-    'djangocms_flash': 'djangocms_flash.migrations_django'
-}
